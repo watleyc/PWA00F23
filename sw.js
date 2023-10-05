1eaa6365bd3beb30b562a664707720ca7c76acbc
watleyc <144378862+watleyc@users.noreply.github.com>
2023-10-05 12:34:21 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,9 +20,9 @@ const PRECACHE_URLS = [
 // The install handler takes care of precaching resources we always need
 self.addEventListener('install',function(event) {
   event.waitUntil(
-    caches.open(PRECACHE).then(function(cache){
+    caches.open(PRECACHE).then(cache){
     return cache.addAll(PRECACHE_URLS);
-    })
+    }
   );
 });
 
